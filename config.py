bb604a66ca4f7d2fad6fdd0a5ff3657f85592d90
aelvismorales <faviotio123@gmail.com>
2022-07-20 11:12:42 -0500

    p

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG=True
-    #WTF_CSRF_CHECK_DEFAULT=False
+    WTF_CSRF_CHECK_DEFAULT=False
     #SQLALCHEMY_DATABASE_URI='postgresql://isnjonskuprehk:07d8bb3abcb665f2611cbf435a79c4dfeeac18604299189722d794802c1971fa@ec2-23-23-182-238.compute-1.amazonaws.com:5432/dbgnihpfcroaus'
     #SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:admin@localhost/sistemaventas'
     SQLALCHEMY_DATABASE_URI='postgresql://qukwtjyqolccui:13de5742221cd4bb36d417bdb9f3191aa770141574fc759a06a55335f385673d@ec2-54-152-28-9.compute-1.amazonaws.com:5432/de53inatidu2qk'
