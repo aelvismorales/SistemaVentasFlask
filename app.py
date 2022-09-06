224022032924396c270883b101e1ae5e76f16e6e
aelvismorales <faviotio123@gmail.com>
2022-09-06 17:19:35 -0500

    Add rollback to session database

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 from flask import Flask, render_template, request,flash,session,redirect,url_for,jsonify,send_file
 from flask_wtf.csrf import CSRFProtect
 from sqlalchemy import desc,asc
@@ -102,19 +103,24 @@ def index():
 @app.route('/crear',methods=['GET','POST'])
 def crear_cuenta():
 	crear_usuario=formularios.CrearCuenta(request.form)
-	if request.method=='POST' and crear_usuario.validate():
-		usuario=Usuario(crear_usuario.username.data,crear_usuario.password.data,crear_usuario.rol.data)
 
-		db.session.add(usuario)
-		db.session.commit()
+	try:
+		if request.method=='POST' and crear_usuario.validate():
+			usuario=Usuario(crear_usuario.username.data,crear_usuario.password.data,crear_usuario.rol.data)
 
-		mensaje='Felicidades por registrarte {}'.format(crear_usuario.username.data)
-		flash(mensaje,category='message')
-	else:
-		mensaje='Hubo un error en crear la cuenta, intente nuevamente'
-		flash(mensaje,category='error')
+			db.session.add(usuario)
+			db.session.commit()
 
-	return render_template('crear_cuenta.html',form_usuario=crear_usuario)
+			mensaje='Felicidades por registrarte {}'.format(crear_usuario.username.data)
+			flash(mensaje,category='message')
+		else:
+			mensaje='Hubo un error en crear la cuenta, intente nuevamente'
+			flash(mensaje,category='error')
+
+		return render_template('crear_cuenta.html',form_usuario=crear_usuario)
+	except ValueError as Error:
+		db.session.rollback()
+		raise Exception(f"Error en la creación de usuario: error {Error}")
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -137,8 +143,8 @@ def login():
 				error_message='Usuario o contraseña no validos!'
 				flash(error_message,category='error')
 		except ValueError as Error:
-			session.rollback()
-			print(Error)
+			db.session.rollback()
+			raise Exception(f"Error en el inicio de usuario: error {Error}")
 
 	return render_template('login.html',form_login=user_form,log=loge)
 
@@ -152,40 +158,47 @@ def logout():
 @app.route('/producto',methods=['GET','POST'])
 def crear_producto():
 	crear_producto=formularios.CrearProducto(request.form)
-	if request.method=='POST' and crear_producto.validate():
-		precio_venta=crear_producto.precio_venta_producto.data
-		producto=Producto(crear_producto.nombre_producto.data,crear_producto.precio_costo_producto.data,precio_venta,crear_producto.stock.data)
+	try:
+		if request.method=='POST' and crear_producto.validate():
+			precio_venta=crear_producto.precio_venta_producto.data
+			producto=Producto(crear_producto.nombre_producto.data,crear_producto.precio_costo_producto.data,precio_venta,crear_producto.stock.data)
 
-		if producto is not None:
-			db.session.add(producto)
-			db.session.commit()
-			succes_message='Se creo el producto "{}"'.format(crear_producto.nombre_producto.data)
-			flash(succes_message,category='message')
-			return render_template('crear_producto.html',producto_form=crear_producto,log=loge)
-		else:
-			error_message='No se pudo crear el producto intente nuevamente'
-			flash(error_message,category='error')
-			
-	return render_template('crear_producto.html',producto_form=crear_producto,log=loge)
-
+			if producto is not None:
+				db.session.add(producto)
+				db.session.commit()
+				succes_message='Se creo el producto "{}"'.format(crear_producto.nombre_producto.data)
+				flash(succes_message,category='message')
+				return render_template('crear_producto.html',producto_form=crear_producto,log=loge)
+			else:
+				error_message='No se pudo crear el producto intente nuevamente'
+				flash(error_message,category='error')
+				
+		return render_template('crear_producto.html',producto_form=crear_producto,log=loge)
+	except ValueError as Error:
+		db.session.rollback()
+		raise Exception(f"Error en la creación del producto: error {Error}")
 
 @app.route('/editar/<int:id>',methods=['GET','POST'])
 def editar_id(id):
 	producto=Producto.query.get(id)
 	editar_producto=formularios.EditarProducto(request.form)
-	if request.method=='POST' and editar_producto.validate(): 	
-		producto.nombre_producto=editar_producto.nombre__producto.data
-		producto.precio_costo_producto=editar_producto.precio_costo_producto.data
-		producto.precio_venta_producto=editar_producto.precio_venta_producto.data
-		producto.stock=editar_producto.stock.data
-		producto.fecha_actualizacion_producto=datetime.now()
+	try:
+		if request.method=='POST' and editar_producto.validate(): 	
+			producto.nombre_producto=editar_producto.nombre__producto.data
+			producto.precio_costo_producto=editar_producto.precio_costo_producto.data
+			producto.precio_venta_producto=editar_producto.precio_venta_producto.data
+			producto.stock=editar_producto.stock.data
+			producto.fecha_actualizacion_producto=datetime.now()
 
-		db.session.add(producto)
-		db.session.commit()
+			db.session.add(producto)
+			db.session.commit()
 
-		succes_message='Se actualizo el producto {}'.format(producto.nombre_producto)
-		flash(succes_message,category='message')
-	return render_template('editar_producto_id.html',editar_form=editar_producto,log=loge,product_name=producto.get_str_nombre(),product_pc=producto.get_str_pc(),product_pv=producto.get_str_pv(),product_stock=producto.get_stock())
+			succes_message='Se actualizo el producto {}'.format(producto.nombre_producto)
+			flash(succes_message,category='message')
+		return render_template('editar_producto_id.html',editar_form=editar_producto,log=loge,product_name=producto.get_str_nombre(),product_pc=producto.get_str_pc(),product_pv=producto.get_str_pv(),product_stock=producto.get_stock())
+	except ValueError as Error:
+		db.session.rollback()
+		raise Exception(f"Error en editar producto: error {Error}")
 
 @app.route('/buscar',methods=['GET','POST'])
 def buscar_producto():
@@ -206,18 +219,22 @@ def buscar_producto():
 @app.route('/eliminarproducto/<string:id>',methods=['GET','POST'])
 def eliminarproducto(id):
 	producto_eliminar=Producto.query.get(id)
-	if producto_eliminar is not None:
-		nombre_elimninar=producto_eliminar.get_str_nombre()
-		db.session.delete(producto_eliminar)
-		db.session.commit()
-		succes_message='Se elimino el producto "{}" satisfactoriamente'.format(nombre_elimninar)
-		flash(succes_message,category='message')
-		return redirect(url_for('.buscar_producto'))
-	else:
-		error_message='El producto no se pudo eliminar intente nuevamente'
-		flash(error_message,category='error')
+	try:
+		if producto_eliminar is not None:
+			nombre_elimninar=producto_eliminar.get_str_nombre()
+			db.session.delete(producto_eliminar)
+			db.session.commit()
+			succes_message='Se elimino el producto "{}" satisfactoriamente'.format(nombre_elimninar)
+			flash(succes_message,category='message')
+			return redirect(url_for('.buscar_producto'))
+		else:
+			error_message='El producto no se pudo eliminar intente nuevamente'
+			flash(error_message,category='error')
 
-	return redirect(url_for('.buscar_producto'))
+		return redirect(url_for('.buscar_producto'))
+	except ValueError as Error:
+		db.session.rollback()
+		raise Exception(f"Error en eliminar producto: error {Error}")
 #-------------------------------------------------------------------------------------------------------#-------------------------------------------------------------------------------------------------------
 @app.route('/crearnotapedido',methods=['GET','POST'])
 def crear_nota_pedido():
@@ -250,18 +267,21 @@ def crear_nota_pedido():
 				session['direccion_comprador']=direccion_comprador
 				session['dni_comprador']=dni_comprador
 				session['telefono_comprador']=telefono_comprador
-				
-				if nota is not None:
-					db.session.add(nota)
-					db.session.commit()
-					session['numero_nota']=nota.get_id()
-					session['fecha_nota']=nota.get_fecha()
-					session['estado_nota']=estado_nota
-					succes_message='Se creo la nota de pedido para {}'.format(nombre_comprador)
-					flash(succes_message,category='message')
-				else:
-					error_message='No se pudo crear la nota de pedido, intentelo nuevamente'
-					flash(error_message,category='error')
+				try:
+					if nota is not None:
+						db.session.add(nota)
+						db.session.commit()
+						session['numero_nota']=nota.get_id()
+						session['fecha_nota']=nota.get_fecha()
+						session['estado_nota']=estado_nota
+						succes_message='Se creo la nota de pedido para {}'.format(nombre_comprador)
+						flash(succes_message,category='message')
+					else:
+						error_message='No se pudo crear la nota de pedido, intentelo nuevamente'
+						flash(error_message,category='error')
+				except ValueError as Error:
+					db.session.rollback()
+					raise Exception(f"Error en la creación de nota de pedido: error {Error}")
 			else:
 					datos_producto_json= json.dumps(session['producto'])
 					comprador_nuevo=Comprador(nombre_comprador,telefono_comprador,direccion_comprador,dni_comprador)
@@ -274,15 +294,19 @@ def crear_nota_pedido():
 					session['direccion_comprador']=direccion_comprador
 					session['dni_comprador']=dni_comprador
 					session['telefono_comprador']=telefono_comprador
-					if nota is not None:
-						db.session.add(nota)
-						db.session.add(comprador_nuevo)
-						db.session.commit()
-						session['numero_nota']=nota.get_id()
-						session['fecha_nota']=nota.get_fecha()
-						session['estado_nota']=estado_nota
-						succes_message='Se creo la nota de pedido para {} y se registro como nuevo comprador'.format(nombre_comprador)
-						flash(succes_message,category='message')
+					try:
+						if nota is not None:
+							db.session.add(nota)
+							db.session.add(comprador_nuevo)
+							db.session.commit()
+							session['numero_nota']=nota.get_id()
+							session['fecha_nota']=nota.get_fecha()
+							session['estado_nota']=estado_nota
+							succes_message='Se creo la nota de pedido para {} y se registro como nuevo comprador'.format(nombre_comprador)
+							flash(succes_message,category='message')
+					except ValueError as Error:
+						db.session.rollback()
+						raise Exception(f"Error en la creación de nota de pedido y registro de usuario: error {Error}")
 	return redirect(url_for('.buscar_producto'))
 
 @app.route('/buscarcompradorbydni',methods=['GET','POST'])
@@ -460,35 +484,43 @@ def editarnota(id):
 	direccion_comprador=request.form.get('direccion_comprador')
 	estado_nota=request.form.get('estado')
 	estado_nota_2=request.form.get('estado2')
-	if nota and nombre_comprador and direccion_comprador and estado_nota and request.method=='POST':
-		nota.nombre_comprador=nombre_comprador
-		nota.direccion_comprador=direccion_comprador
-		nota.estado=estado_nota+estado_nota_2
-		if estado_nota in ['cancelado','cancelado-entregado','cancelado-por-recoger','cancelado-']:
-			nota.fecha_creacion=datetime.now()
-		elif estado_nota in ['cancelado-VISA','cancelado-VISA-entregado','cancelado-VISA-por-recoger','cancelado-VISA-']:
-			nota.fecha_creacion=datetime.now()
-		else:
-			pass
-		db.session.add(nota)
-		db.session.commit()
-		succes_message='Se actualizo la nota de pedido de {}'.format(nota.nombre_comprador)
-		flash(succes_message,category='message')
+	try:
+		if nota and nombre_comprador and direccion_comprador and estado_nota and request.method=='POST':
+			nota.nombre_comprador=nombre_comprador
+			nota.direccion_comprador=direccion_comprador
+			nota.estado=estado_nota+estado_nota_2
+			# if estado_nota in ['cancelado','cancelado-entregado','cancelado-por-recoger','cancelado-']:
+			# 	nota.fecha_creacion=datetime.now()
+			# elif estado_nota in ['cancelado-VISA','cancelado-VISA-entregado','cancelado-VISA-por-recoger','cancelado-VISA-']:
+			# 	nota.fecha_creacion=datetime.now()
+			# else:
+			# 	pass
+			db.session.add(nota)
+			db.session.commit()
+			succes_message='Se actualizo la nota de pedido de {}'.format(nota.nombre_comprador)
+			flash(succes_message,category='message')
+			return render_template('editar_nota_id.html',nota=nota,log=loge)
 		return render_template('editar_nota_id.html',nota=nota,log=loge)
-	return render_template('editar_nota_id.html',nota=nota,log=loge)
+	except ValueError as Error:
+		db.session.rollback()
+		raise Exception(f"Error en la edicion de la nota de pedido: error {Error}")
 
 @app.route('/anularnota/<string:id>',methods=['GET','POST'])
 def anularnota(id):
 	nota=Nota_de_Pedido.query.get(id)
 	print('llego2')
-	if nota:
-		nota.estado='ANULADO-'
-		db.session.add(nota)
-		db.session.commit()
-		succes_message='Se ANULO la nota de pedido {}'.format(nota.id)
-		flash(succes_message,category='message')
+	try:
+		if nota:
+			nota.estado='ANULADO-'
+			db.session.add(nota)
+			db.session.commit()
+			succes_message='Se ANULO la nota de pedido {}'.format(nota.id)
+			flash(succes_message,category='message')
+			return redirect(request.referrer)
 		return redirect(request.referrer)
-	return redirect(request.referrer)
+	except ValueError as Error:
+		db.session.rollback()
+		raise Exception(f"Error en anular nota de pedido: error {Error}")
 
 
 #-------------------------------------------------------------------------------------------------------#-------------------------------------------------------------------------------------------------------
@@ -499,18 +531,21 @@ def crearcomprador():
 
 	if request.method=='POST' and comprador.validate():
 		existe_comprador=Comprador.query.filter_by(nombre_comprador=comprador.nombre_comprador.data).first()
-
-		if existe_comprador is None:
-			nuevo_comprador=Comprador(comprador.nombre_comprador.data,comprador.numero_telefono.data,comprador.tipo_comprador.data,comprador.dni.data,comprador.direccion_comprador.data)
-		
-			db.session.add(nuevo_comprador)
-			db.session.commit()
+		try:
+			if existe_comprador is None:
+				nuevo_comprador=Comprador(comprador.nombre_comprador.data,comprador.numero_telefono.data,comprador.tipo_comprador.data,comprador.dni.data,comprador.direccion_comprador.data)
 			
-			succes_message='Se creo el usuario {}'.format(nuevo_comprador.nombre_comprador)
-			flash(succes_message,message='message')
-		else:
-			error_message='No se puede crear el comprador, este ya se encuentra registrado'
-			flash(error_message,category='error')
+				db.session.add(nuevo_comprador)
+				db.session.commit()
+				
+				succes_message='Se creo el usuario {}'.format(nuevo_comprador.nombre_comprador)
+				flash(succes_message,message='message')
+			else:
+				error_message='No se puede crear el comprador, este ya se encuentra registrado'
+				flash(error_message,category='error')
+		except ValueError as Error:
+			db.session.rollback()
+			raise Exception(f"Error en la creación de comprador: error {Error}")
 	return render_template('crear_comprador.html',form_comprador=comprador,log=loge)
 
 @app.route('/editarcomprador/<string:id>',methods=['GET','POST'])
@@ -523,35 +558,43 @@ def editarcomprador(id):
 	comprador_dni=request.form.get('dni')
 
 	if request.method=='POST':
-		if comprador_encontrado is not None:
-			comprador_encontrado.nombre_comprador=comprador_nombre
-			comprador_encontrado.numero_telefono_comprador=comprador_numero_telefono
-			comprador_encontrado.tipo_comprador=comprador_tipo_comprador
-			comprador_encontrado.direccion_comprador=comprador_direccion
-			comprador_encontrado.dni=comprador_dni
+		try:
+			if comprador_encontrado is not None:
+				comprador_encontrado.nombre_comprador=comprador_nombre
+				comprador_encontrado.numero_telefono_comprador=comprador_numero_telefono
+				comprador_encontrado.tipo_comprador=comprador_tipo_comprador
+				comprador_encontrado.direccion_comprador=comprador_direccion
+				comprador_encontrado.dni=comprador_dni
 
-			db.session.add(comprador_encontrado)
-			db.session.commit()
+				db.session.add(comprador_encontrado)
+				db.session.commit()
 
-			success_message='Se actualizo correctamente al comprador {}'.format(comprador_encontrado.nombre_comprador)
-			flash(success_message,category='message')
-		else:
-			error_message='No se pudo actualizar la informacion del comprador,intente nuevamente'
-			flash(error_message,category='error')
+				success_message='Se actualizo correctamente al comprador {}'.format(comprador_encontrado.nombre_comprador)
+				flash(success_message,category='message')
+			else:
+				error_message='No se pudo actualizar la informacion del comprador,intente nuevamente'
+				flash(error_message,category='error')
+		except ValueError as Error:
+			db.session.rollback()
+			raise Exception(f"Error en editar los datos del comprador: error {Error}")
 	return render_template('editar_comprador.html',log=loge,form_compra=comprador_encontrado)
 @app.route('/eliminarcomprador/<string:id>',methods=['GET','POST'])
 def eliminarcomprador(id):
 	comprador_encontrado=Comprador.query.get(id)
-	if comprador_encontrado is not None:
-		nombre_comprador=comprador_encontrado.get_nombre()
-		db.session.delete(comprador_encontrado)
-		db.session.commit()
-		success_message='Se elimino correctamente al comprador {}'.format(nombre_comprador)
-		flash(success_message,category='message')
-	else:
-		error_message='No se pudo eliminar el comprador'
-		flash(error_message,category='error')
-	return redirect(url_for('.vercompradores'))
+	try:
+		if comprador_encontrado is not None:
+			nombre_comprador=comprador_encontrado.get_nombre()
+			db.session.delete(comprador_encontrado)
+			db.session.commit()
+			success_message='Se elimino correctamente al comprador {}'.format(nombre_comprador)
+			flash(success_message,category='message')
+		else:
+			error_message='No se pudo eliminar el comprador'
+			flash(error_message,category='error')
+		return redirect(url_for('.vercompradores'))
+	except ValueError as Error:
+		db.session.rollback()
+		raise Exception(f"Error en eliminar a comprador: error {Error}")
 
 
 @app.route('/vercompradores',methods=['GET','POST'])
@@ -700,8 +743,8 @@ def imprimire(id):
 		telefono_comprador=''
 		dni_comprador=''
 	else:
-		telefono_comprador=nota.notasdepedidos.numero_telefono_comprador
-		dni_comprador=nota.notasdepedidos.dni
+		telefono_comprador=nota.notasdepedidos.get_telefono()
+		dni_comprador=nota.notasdepedidos.get_dni()
 	return render_template('imprimirid.html',nota=nota,notas=notas,telefono=telefono_comprador,dni=dni_comprador)
 
 #-------------------------------------------------------------------------------------------------------#-------------------------------------------------------------------------------------------------------
