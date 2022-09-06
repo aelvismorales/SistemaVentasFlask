17e4f8d6a2f7e474ef3577470b06104435b2ae4b
aelvismorales <faviotio123@gmail.com>
2022-09-06 17:21:28 -0500

    Add rollback to session database

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config(object):
     SECRET_KEY='123447a47f563e90fe2db0f56b1b17be62378e31b7cfd3adc776c59ca4c75e2fc512c15f69bb38307d11d5d17a41a7936789'
 
 class DevelopmentConfig(Config):
-    #DEBUG=environ.get('DEBUGGER')
-    DEBUG=True
+    DEBUG=environ.get('DEBUGGER')
+    #DEBUG=True
     WTF_CSRF_CHECK_DEFAULT=False
-    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:admin@localhost/sistemaventas'
-    #SQLALCHEMY_DATABASE_URI=environ.get('DATABASE_URL').replace('postgres','postgresql')
+    #SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:admin@localhost/sistemaventas'
+    SQLALCHEMY_DATABASE_URI=environ.get('DATABASE_URL').replace('postgres','postgresql')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
